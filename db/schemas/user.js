9597da78f577cf6a9d64e0448fbbeb5a421285e0
Yoonjae Choi <dbswo9795@gmail.com>
2023-04-18 11:34:59 +0900

    back-end 초기 개발 환경 구축

---
+++ db/schemas/user.js
@@ -0,0 +1,36 @@
+const {Schema} = require("mongoose");
+
+//유저정보
+const UserSchema = new Schema ({
+  email: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: false,
+  },
+  cart: {
+    type: Array,
+    required: false,
+  }
+})
+
+module.exports = UserSchema;
