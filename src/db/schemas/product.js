2ac5f74de710ede5e1adabfc56711c828ed3b83f
[SW4/2반] 성치호 <clghdi9898@gmail.com>
2023-04-18 14:34:01 +0900

    fix: product.js product 스키마 수정 shortId 추가

--- src/db/schemas/product.js
+++ src/db/schemas/product.js
@@ -1,31 +1,42 @@
 const { Schema } = require('mongoose');
+const shortId = require('./types/short-id');
+
+// 순서대로 id(shortId), 상품명, 가격, 카테고리, 설명, 요약, 회사, 재고
 
 const ProductSchema = new Schema ({
+  shortId,
+  //이름
   name: {
     type: String,
     required: true,
     unique: true
   },
+  //가격
   price: {
     type: Number,
     required: true,
   },
+  //카테고리
   category: {
     type: String,
     required: true,
   },
-  description: {
+  //설명
+  description: { 
     type: String,
     required: true,
   },
+  //요약
   summary: {
     type: String,
     required: true,
   },
+  //회사
   company: {
     type: String,
     required: true,
   },
+  //재고
   stock: {
     type: Number,
     required: true,
