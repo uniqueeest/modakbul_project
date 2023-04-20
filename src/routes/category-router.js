051354ddb38fd6d67d2bdc8f7a7fe9cb5ea9b49b
[SW4/2반] 성치호 <clghdi9898@gmail.com>
2023-04-20 23:53:27 +0900

    feat: 카테고리 생성 기능구현 category-service.js에서 카테고리 생성 메서드 구현,  category-router.js 에서 카테고리 생성 api 구현

---
+++ src/routes/category-router.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+const CategoryService = require('../service/category-service');
+const categoryRouter = Router();
+
+// 카테고리 생성
+categoryRouter.post('/add', async (req, res) => {
+    const categoryInfo = req.body;
+    try {
+        const category = await CategoryService.addCategory(categoryInfo);
+
+        res.status(200).json({
+            message: '카테고리 추가 성공!',
+            categories: category
+          });
+    } catch (err) {
+        console.log(err);
+        res.status(400).send(`${err}`);
+    }  
+});
+
+module.exports = categoryRouter;
---
+++ src/service/category-service.js
@@ -0,0 +1,29 @@
+const { Category } = require('../db/models/category-model');
+
+const addCategory = async (categoryInfo) => {
+    try {
+        // categoryInfo로 가져온 정보들을 구조분해할당
+        const {major, minor} = categoryInfo;
+
+        // 카테고리이름 중복 체크
+        const majorDuplicate = await Category.findOne({ major });
+
+        if (majorDuplicate) {
+            throw new Error('이미 등록된 상품입니다.');
+          }
+
+        // 카테고리 생성
+        const newCategory = new Category({
+            major,
+            minor
+        });
+
+        const savedCategory = await newCategory.save();
+
+        return savedCategory;
+    } catch (err) {
+        throw new Error(`카테고리 등록에 실패했습니다. ${err.message}`);
+    }
+};
+
+module.exports = { addCategory };
