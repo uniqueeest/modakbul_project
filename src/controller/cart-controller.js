1e5120b998f1879779bf0352d2edac4d8b3d7d75
[SW4/2반] 노은탁 <popcron13@gmail.com>
2023-04-24 15:21:16 +0900

    controller 내부 파일 로컬라이징용 커밋입니다.

--- src/controller/cart-controller.js
+++ src/controller/cart-controller.js
@@ -1,51 +1,60 @@
 const cartService = require('../service/cart-service');
 
-const cartController = {
-    //장바구니 추가 컨트롤러
-    async cartPost (req, res) {
-        try{
-            const userIdKey = req.user.id;
-            const cartAdd = req.body;
 
-            await cartService.postCart(userIdKey, cartAdd);
-            res.status(200).send('장바구니에 등록되었습니다.');
-        } catch (err) {
-            res.status(400).send(`${err}`);
-        };
-    },
-    //장바구니 조회 컨트롤러
-    async cartGet (req, res) {
-        try{
-            const userIdKey = req.user.id;
-            const cartItems = await cartService.presentCart(userIdKey);
-            res.status(200).send(cartItems);
-        } catch (err) {
-            res.status(400).send(`${err}`);
-        };
-    },
-    //장바구니 개별 삭제 컨트롤러
-    async cartDeleteOne (req, res) {
-        try {
-            const userIdKey = req.user.id;
-            const cartId = req.params;
+//장바구니 추가 컨트롤러
+const cartPost = async (req, res) => {
+    try{
+        const userIdKey = req.user.id;
+        const cartAdd = req.body;
 
-            await cartService.removeCart(userIdKey, cartId);
-            res.status(200).send('장바구니 삭제에 성공했습니다.');
-        } catch (err) {
-            res.status(400).send(`${err}`);
-        }
-    },
-    //장바구니 전체 삭제 컨트롤러
-    async cartDeleteAll (req, res) {
-        try {
-            const userIdKey = req.user.id;
+        await cartService.postCart(userIdKey, cartAdd);
+        res.status(200).send('장바구니에 등록되었습니다.');
+    } catch (err) {
+        res.status(400).send(`${err}`);
+    };
+}
 
-            await cartService.removeAllCart(userIdKey);
-            res.status(200).send('장바구니 전체 삭제에 성공했습니다.');
-        } catch (err) {
-            res.status(400).send(`${err}`);
-        }
+//장바구니 조회 컨트롤러
+const cartGet = async (req, res) => {
+    try{
+        const userIdKey = req.user.id;
+        const cartItems = await cartService.presentCart(userIdKey);
+        res.status(200).send(cartItems);
+    } catch (err) {
+        res.status(400).send(`${err}`);
+    };
+}
+
+//장바구니 개별 삭제 컨트롤러
+const cartDeleteOne = async (req, res) => {
+    try {
+        const userIdKey = req.user.id;
+        const cartId = req.params;
+
+        await cartService.removeCart(userIdKey, cartId);
+        res.status(200).send('장바구니 삭제에 성공했습니다.');
+    } catch (err) {
+        res.status(400).send(`${err}`);
+    }
+}
+
+//장바구니 전체 삭제 컨트롤러
+const cartDeleteAll = async (req, res) => {
+    try {
+        const userIdKey = req.user.id;
+
+        await cartService.removeAllCart(userIdKey);
+        res.status(200).send('장바구니 전체 삭제에 성공했습니다.');
+    } catch (err) {
+        res.status(400).send(`${err}`);
     }
 };
+
+const cartController = {
+    cartPost,
+    cartGet,
+    cartDeleteOne,
+    cartDeleteAll
+}
 
 module.exports = cartController;
