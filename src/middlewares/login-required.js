bb426e70f82257b5867bbd5b1e9b1d8d50df350f
[SW4/2반] 노은탁 <popcron13@gmail.com>
2023-04-24 21:04:38 +0900

    feature/cart 파트 코드 누락 부분 추가 수정 커밋 입니다.

--- src/middlewares/login-required.js
+++ src/middlewares/login-required.js
@@ -15,7 +15,7 @@ const authMiddleware = (req, res, next) => {
   try {
 
     const decodedToken = jwt.verify(token, "jwt-secret");
-    req.user = decodedToken;
+    req.user =decodedToken;
     const userId = decodedToken.userId;
     req.currentUserId = userId;
     next();
