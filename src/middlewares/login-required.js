4017369a093139476110b2e5ff25377fa5cba032
[SW4/2반] 노은탁 <popcron13@gmail.com>
2023-04-25 18:45:46 +0900

    JWT 인증 토큰 관련 수정 커밋입니다.

--- src/middlewares/login-required.js
+++ src/middlewares/login-required.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 
 //jwt 검증 미들웨어
 const authMiddleware= (req, res, next) => {
-  const token= req.headers['x-access-token'] || req.query.token;
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; //"Bearer 토큰 값"에서 토큰 값만 추출
 
   //토큰이 존재하지 않을 때
   if (!token) {
