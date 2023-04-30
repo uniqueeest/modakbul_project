6405aeb1bb34711c1e8046000758510f77ac60e6
Yoonjae Choi <dbswo9795@gmail.com>
2023-05-01 01:59:40 +0900

    fix: 관리자 주문 관련 api 수정

--- src/routes/order-router.js
+++ src/routes/order-router.js
@@ -5,7 +5,7 @@ const authMiddleware = require("../middlewares/login-required");
 const adminMiddleware = require("../middlewares/admin-required");
 
 //관리자) 주문 정보 조회
-orderRouter.get("/admin/:adminId", authMiddleware, adminMiddleware, orderController.adminGetOrder);
+orderRouter.get("/admin", authMiddleware, adminMiddleware, orderController.adminGetOrder);
 
 //비회원 주문 정보 조회
 orderRouter.get("/:orderNumber", orderController.nonMemberGetOrder);
@@ -20,7 +20,7 @@ orderRouter.post("/", authMiddleware, orderController.createOrder);
 orderRouter.post("/nonmember", orderController.createNonMemberOrder);
 
 //관리자) 주문 수정
-orderRouter.patch("/:adminId/:orderId", authMiddleware, adminMiddleware, orderController.adminUpdateOrder);
+orderRouter.patch("/admin/:orderId", authMiddleware, adminMiddleware, orderController.adminUpdateOrder);
 
 //주문 수정
 orderRouter.patch("/:orderId", authMiddleware, orderController.updateOrder);
@@ -32,6 +32,6 @@ orderRouter.patch("/nonmember/:orderId", orderController.updateOrder);
 orderRouter.delete("/:orderId", orderController.deleteOrder);
 
 //관리자) 주문 취소
-orderRouter.delete("/:adminId/:orderId", authMiddleware, adminMiddleware, orderController.adminDeleteOrder);
+orderRouter.delete("/admin/:orderId", authMiddleware, adminMiddleware, orderController.adminDeleteOrder);
 
 module.exports = orderRouter;
